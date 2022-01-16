008a835d277f6f576c2ca8d09492ac81c9458b20
于明明 <mingming.yu@hanshow.com>
2022-01-16 21:02:26 +0800

    rename dir

--- main.go
+++ main.go
@@ -1,11 +1,84 @@
 package main
 
 import (
-	"github.com/spf13/afero"
+	"flag"
+	"fmt"
+	"log"
 	"os"
-	"vstruct/cmd"
+	"vstruct/src/vstruct"
 )
 
+// Version is the version of `accessory`, injected at build time.
+var Version = ""
+
+// newUsage returns a function to replace default usage function of FlagSet.
+func newUsage(flags *flag.FlagSet) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "Usage of vstruct:\n")
+		fmt.Fprintf(os.Stderr, "\tvstruct [flags] [directory]\n")
+		fmt.Fprintf(os.Stderr, "Flags:\n")
+		flags.PrintDefaults()
+	}
+}
+
+func isDir(name string) bool {
+	info, err := os.Stat(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return info.IsDir()
+}
+
+// execute a whole process of generating accessor codes.
 func main() {
-	cmd.Execute(afero.NewOsFs(), os.Args)
+	args := os.Args
+	log.SetFlags(0 | log.Lshortfile)
+	log.SetPrefix("vstruct: ")
+
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.Usage = newUsage(flags)
+	version := flags.Bool("version", false, "show the version of vstruct")
+	structName := flags.String("struct", "", "struct name; must be set")
+	receiver := flags.String("receiver", "", "receiver name; default first letter of struct name")
+	output := flags.String("output", "", "output file name; default <package_name>_<struct_name>_vstruct.go")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		flags.Usage()
+		os.Exit(1)
+	}
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "accessory version: %s\n", Version)
+		os.Exit(0)
+	}
+
+	if structName == nil || len(*structName) == 0 {
+		flags.Usage()
+		os.Exit(1)
+	}
+
+	var dir string
+	if cliArgs := flags.Args(); len(cliArgs) > 0 {
+		dir = cliArgs[0]
+	} else {
+		// Default: process whole package in current directory.
+		dir = "."
+	}
+
+	if !isDir(dir) {
+		fmt.Fprintln(os.Stderr, "Specified argument is not a directory.")
+		flags.Usage()
+		os.Exit(1)
+	}
+
+	pkg, err := vstruct.ParsePackage(dir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		flags.Usage()
+		os.Exit(1)
+	}
+
+	if err = vstruct.Generate(pkg, *structName, *output, *receiver); err != nil {
+		log.Fatal(err)
+	}
 }
